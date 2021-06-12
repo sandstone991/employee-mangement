6a537f44f3e5a2034941f6c92093df6078d4c146
sandstone991 <61756360+sandstone991@users.noreply.github.com>
2021-06-12 10:04:01 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <string.h>
+#define SIZE 20
+/***********************************************************************************************************************/
+//Node struct
+
+/***********************************************************************************************************************/
+//Declare gloabal variables 
+
+/***********************************************************************************************************************/
+//Declare functions
+
+//*****
+
+//QUEUE FUNCTIONS DECLARATION SECTION START
+
+//QUEUE FUNCTIONS DECLARATION SECTION END
+
+//******
+
+//FILE FUNCTIONS DECLARATION START
+
+//FILE FUNCTIONS DECLARATION END
+
+//******
+
+//MISC FUNCTION DECLARATION START
+
+//MISC FUNCTION DECLARATION END
+
+//******
+
+/***********************************************************************************************************************/
+//Main funtion
+
+main()
+{
+  
+}
+/***********************************************************************************************************************/
+//Functions definition
+
+//****************
+
+//QUEUE FUNCTIONS DEFINTION START
+
+//QUEUE FUNCTIONS DEFINTION END
+
+//****************
+
+//MISC FUNCTIONS DEFINTION START
+
+//MISC FUNCTIONS DEFINITION END
+
+//****************
+
+//FILE FUNCTIONS DEFINTION START
+
+//FILE FUNCTIONS DEFINITON END
+
+//****************
+
+
+
+
